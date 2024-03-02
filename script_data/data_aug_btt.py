4045705c890a8703bfcf012c66f2e183dc7c6dc9
Abhinav Kumar <abby37kapoor@gmail.com>
2024-03-02 18:28:49 +0100

    btt augmentation code updated

--- script_data/data_aug_btt.py
+++ script_data/data_aug_btt.py
@@ -1,3 +1,6 @@
+import pdb
+
+import pandas as pd
 import torch
 from nltk.translate.bleu_score import sentence_bleu
 from sacrebleu.metrics import BLEU
@@ -32,34 +35,156 @@ bleu = BLEU()
 # pipe_fr_to_en = pipeline("translation_fr_to_en", model="facebook/nllb-200-1.3B", device=device,
 #                          framework='pt',)
 
+train_25_aug = pd.read_csv(
+    "/cluster/work/sachan/abhinav/text_complexity/data/syn_aug_125.csv")
+
+train_25_aug = train_25_aug[['writing_id', 'sentences', 'cefr_numeric']]
+
+# bcp 47 code https://github.com/facebookresearch/flores/blob/main/toxicity/README.md
+list_of_translation_lang = ['zho_Hans', 'spa_Latn', 'arb_Arab', 'fra_Latn', 'deu_Latn',
+                            'rus_Cyrl', 'jpn_Jpan', 'por_Latn', 'hin_Deva', 'ita_Latn']
+
+# Chinese (Mandarin)
+# Spanish
+# Arabic
+# French
+# German
+# Russian
+# Japanese
+# Portuguese
+# Hindi
+# Italian
+
 
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 tokenizer_eng = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="eng_Latn",
                                               cache_dir="/cluster/work/sachan/abhinav/model/fb/",
                                               device=device)
+tokenizer_zho = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="zho_Hans",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_spa = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="spa_Latn",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_arb = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="arb_Arab",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_fra = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="fra_Latn",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_deu = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="deu_Latn",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_rus = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="rus_Cyrl",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_jpn = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="jpn_Jpan",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_por = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="por_Latn",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_hin = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="hin_Deva",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+tokenizer_ita = NllbTokenizer.from_pretrained("facebook/nllb-200-1.3B", src_lang="ita_Latn",
+                                              cache_dir="/cluster/work/sachan/abhinav/model/fb/",
+                                              device=device)
+
+tokenizer_dict = {
+    "zho_Hans": tokenizer_zho,
+    "spa_Latn": tokenizer_spa,
+    "arb_Arab": tokenizer_arb,
+    "fra_Latn": tokenizer_fra,
+    "deu_Latn": tokenizer_deu,
+    "rus_Cyrl": tokenizer_rus,
+    "jpn_Jpan": tokenizer_jpn,
+    "por_Latn": tokenizer_por,
+    "hin_Deva": tokenizer_hin,
+    "ita_Latn": tokenizer_ita,
+    "eng_Latn": tokenizer_eng
+}
+
+# https://huggingface.co/facebook/nllb-200-1.3B/tree/main
 model = AutoModelForSeq2SeqLM.from_pretrained(
     "facebook/nllb-200-1.3B", cache_dir="/cluster/work/sachan/abhinav/model/fb/")
 model.to(device)
 
-article = "Information about training algorithms, parameters, fairness constraints or other applied approaches, and features."
-ref = [[article]]
-inputs = tokenizer_eng(article, return_tensors="pt")
-inputs.to(device)
+data_rows = []
+for _, row in train_25_aug.iterrows():
+    sentence = row['sentences']
+    ref = [[sentence]]
+    input_tokenize_eng = tokenizer_eng(sentence, return_tensors="pt")
+    input_tokenize_eng.to(device)
+    highest_bleu = float('-inf')
+    second_highest_bleu = float('-inf')
+    highest_lang_sent = second_highest_lang_sent = ''
+    highest_lang = second_highest_lang = ''
+    for translation_lang in list_of_translation_lang:
+        sentence_tokens = model.generate(
+            **input_tokenize_eng, forced_bos_token_id=tokenizer_eng.lang_code_to_id[translation_lang], max_length=200)
+        translated_tokenizer = tokenizer_dict[translation_lang]
+        translated_sentence = translated_tokenizer.batch_decode(
+            sentence_tokens, skip_special_tokens=True)[0]
+        translated_input = translated_tokenizer(
+            translated_sentence, return_tensors="pt")
+        translated_input.to(device)
+        eng_translated_tokens = model.generate(
+            **translated_input, forced_bos_token_id=tokenizer_eng.lang_code_to_id['eng_Latn'], max_length=200)
+        eng_translated_sentence = tokenizer_eng.batch_decode(
+            eng_translated_tokens, skip_special_tokens=True)[0]
+        candidate = [eng_translated_sentence]
+        bleu_score = bleu.corpus_score(candidate, ref)
+        bleu_score = bleu_score.score
+        if bleu_score > highest_bleu:
+            second_highest_bleu = highest_bleu
+            second_highest_lang = highest_lang
+            second_highest_lang_sent = highest_lang_sent
+            highest_bleu = bleu_score
+            highest_lang = translation_lang
+            highest_lang_sent = eng_translated_sentence
+        elif bleu_score > second_highest_bleu:
+            second_highest_bleu = bleu_score
+            second_highest_lang = translation_lang
+            second_highest_lang_sent = eng_translated_sentence
+        # print('translation_lang', translation_lang)
+        # print('translated_sentence', translated_sentence)
+        # print('tokens', input_tokenize_eng,
+        #       translated_input, eng_translated_tokens)
+        # print('eng_translated_sentence', eng_translated_sentence)
+        # print('bleu_score', bleu_score)
 
-list_of_translation_lang = ["deu_Latn", "fra_Latn", "ita_Latn", "por_Latn", "spa_Latn", "rus_Cyrl",
-                            "ara_Arab", "zho_Hans", "jpn_Hani", "tur_Latn", "heb_Hebr", "hin_Deva", "ben_Beng"]
+    data = [row['writing_id'], sentence, row['cefr_numeric'], highest_lang_sent,
+            highest_lang, second_highest_lang_sent, second_highest_lang]
+    data_rows.append(data)
+    pdb.set_trace()
+data_df = pd.DataFrame(data_rows, columns=['writing_id', 'sentences', 'cefr_numeric',
+                       'highest_lang_sent', 'highest_lang', 'second_highest_lang_sent', 'second_highest_lang'])
 
-for translation_lang in list_of_translation_lang:
-    translated_tokens = model.generate(
-        **inputs, forced_bos_token_id=tokenizer_eng.lang_code_to_id[translation_lang], max_length=30)
-    print(translation_lang)
-    print(tokenizer_eng.batch_decode(
-        translated_tokens, skip_special_tokens=True)[0])
-    translated_article = tokenizer_eng.batch_decode(
-        translated_tokens, skip_special_tokens=True)[0]
-    candidate = [translated_article]
-    bleu_score = bleu.corpus_score(candidate, ref)
-    print(bleu_score.score)
-    print("\n")
+data_df.to_csv(
+    '/cluster/work/sachan/abhinav/text_complexity/data/translated_sentences.csv', index=False)
 
+# article = "Information about training algorithms, parameters, fairness constraints or other applied approaches, and features."
+# ref = [[article]]
+# inputs = tokenizer_eng(article, return_tensors="pt")
+# inputs.to(device)
+
+# for translation_lang in list_of_translation_lang:
+#     translated_tokens = model.generate(
+#         **inputs, forced_bos_token_id=tokenizer_eng.lang_code_to_id[translation_lang], max_length=100)
+#     print(translation_lang)
+#     print(tokenizer_eng.batch_decode(
+#         translated_tokens, skip_special_tokens=True)[0])
+#     translated_article = tokenizer_eng.batch_decode(
+#         translated_tokens, skip_special_tokens=True)[0]
+#     candidate = [translated_article]
+#     bleu_score = bleu.corpus_score(candidate, ref)
+#     print(bleu_score.score)
+#     print("\n")
+#     pdb.set_trace()
+# tt = tokenizer_zho(translated_article, return_tensors="pt")
+# tokenizer_zho.batch_decode(tt, skip_special_tokens=True)[0]
+# aa = model.generate(**translated_tokens,
+#                     forced_bos_token_id=tokenizer_eng.lang_code_to_id['eng_Latn'], max_length=30)
+# tokenizer_zho.batch_decode(aa, skip_special_tokens=True)[0]
 # srun -n 1 --cpus-per-task=4 --time=4:00:00 --job-name="learn1" --mem-per-cpu=16384 --gpus=1 --gres=gpumem:24G --pty python3 text_complexity/script_data/data_aug_btt.py
